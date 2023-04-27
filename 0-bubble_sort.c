6513823fda930d4ea7b286d1788e9033872737a9
Imhandegbelo <abramgeo5@gmail.com>
2023-04-26 17:16:48 -0700

    added bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts array of integers in ascending order with the bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; j++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 0;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Sorting Alog0rithms
